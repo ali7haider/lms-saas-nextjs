2900bfa06fba60bf555128484648c0dbb1ceef82
Ali Haider <ali.haider@pentaloop.com>
2026-04-08 17:18:01 +0500

    feat: integrate email OTP functionality with resend service and update environment variables

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,9 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { prisma } from "./prisma";
 import { env } from "./env";
+import { emailOTP } from "better-auth/plugins";
+import { resend } from "./resend";
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
@@ -13,4 +16,16 @@ export const auth = betterAuth({
       clientSecret: env.GITHUB_CLIENT_SECRET,
     },
   },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        const { data, error } = await resend.emails.send({
+          from: "Acme <onboarding@resend.dev>",
+          to: [email],
+          subject: "Hello world",
+          html: `<strong>Your OTP is ${otp}</strong>`,
+        });
+      },
+    }),
+  ],
 });
