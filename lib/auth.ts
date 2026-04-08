bfc5a586d659fd7525293368dad54ce0c2242cb6
Ali Haider <ali.haider@pentaloop.com>
2026-04-08 15:39:55 +0500

    feat: enhance authentication setup with environment variables and add user/session models

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,8 +2,15 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { prisma } from "./prisma";
+import { env } from "./env";
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
 });
